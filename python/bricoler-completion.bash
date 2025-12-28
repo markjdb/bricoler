19515fd4da8a87f779af569e3bd11e0bdbee1d46
Mark Johnston <markj@FreeBSD.org>
2025-12-28 15:04:17 +0000

    Add a new implementation of bricoler
    This time, in python.  The extensive standard library and affordances
    related to types make this much more pleasant than lua, at the cost of
    speed, but that's the right tradeoff.
    
    The underlying implementation is quite similar.  There are some changes
    to the command-line syntax:
    1) There are no more verbs, just run `bricoler <task> <flags> <params>`.
       Some flags can change behaviour, e.g., one can have
    
       $ bricoler freebsd-src-build -s
    
       to show the parameter bindings.
    2) Parameter syntax is a bit shorter, instead of
       `--param <task>:<param>=<val>`, use `--<task>/<param>=<val>`.
    
    This is far from being complete, I am still porting over much of the
    meat from the lua implementation.  But most of the core is there.  I
    also added a new feature: one can now define aliases for useful
    commands, so I can, for example, make "bricoler foo" run a task with a
    whole set of parameters.  Aliases are stored in bricoler.json at the
    root of the workdir.  To define an alias, run:
    
       $ bricoler <task> <params> -a <alias>
    
    Now you can run `bricoler <alias>` instead of
    `bricoler <task> <params>`.
    
    This attempt is far enough along that I'll probably finish it and
    replace the lua version, so let's start checkpointing.

---
+++ python/bricoler-completion.bash
@@ -0,0 +1,61 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) Mark Johnston <markj@FreeBSD.org>
+#
+
+_bricoler_completions()
+{
+    local all_opts cmd cur params prev task tasks
+
+    cmd=$1
+    options="--show --list --workdir"
+    tasks=$(BRICOLER_ARGCOMPLETE=1 "$cmd" -l 2>/dev/null)
+
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD - 1]}"
+
+    # Do all options so far start with "-"?
+    all_opts=1
+    for word in "${COMP_WORDS[@]:1}"; do
+        case "$word" in
+        -*|$cur)
+            ;;
+        *)
+            all_opts=0
+            task="$word"
+            params=$(BRICOLER_ARGCOMPLETE=1 "$cmd" $task -l 2>/dev/null | awk '{print "--"$1"="}')
+            break
+            ;;
+        esac
+    done
+
+    if [ $all_opts -eq 1 ]; then
+        COMPREPLY=($(compgen -W "$tasks" -- "$cur"))
+        return 0
+    else
+        compopt -o nospace
+        COMPREPLY=($(compgen -W "$params" -- "$cur"))
+        return 0
+    fi
+
+    params=""
+    if [ "$COMP_CWORD" -ge 2 -a "${COMP_WORDS[1]}" = "run" ]; then
+        # Assume that the argument following "run" is the task name.
+        # This doesn't have to be true, maybe we should try harder...
+        params=$(BRICOLER_ARGCOMPLETE=1 "$cmd" -l ${COMP_WORDS[2]} 2>/dev/null | awk '{print $1"="}')
+    fi
+
+    case $prev in
+    -w|--workdir)
+        COMPREPLY=($(compgen -d -- "$cur"))
+        return 0
+        ;;
+    *)
+        COMPREPLY=($(compgen -W "$options" -- "$cur"))
+        return 0
+        ;;
+    esac
+}
+
+complete -F _bricoler_completions bricoler
---
+++ python/util.py
@@ -0,0 +1,49 @@
+#
+# Copyright (c) Mark Johnston <markj@FreeBSD.org>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+import functools
+import os
+import subprocess
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Dict, List, Optional
+
+
+@contextmanager
+def chdir(dir: Path):
+    old_dir = Path.cwd()
+    dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(dir)
+    try:
+        yield
+    finally:
+        os.chdir(old_dir)
+
+
+def sysctl(name: str) -> str:
+    result = run_cmd(["sysctl", "-n", name], capture_output=True, text=True)
+    return result.stdout.strip()
+
+
+@functools.cache
+def host_machine() -> str:
+    return sysctl("hw.machine") + "/" + sysctl("hw.machine_arch")
+
+
+def run_cmd(cmd: List[str], *args, env: Optional[Dict[str, str]] = None, **kwargs):
+    if not os.getenv("BRICOLER_ARGCOMPLETE"):
+        print(f"EXEC: '{' '.join(cmd)}'")
+    if env is not None:
+        tmp = os.environ.copy()
+        tmp.update(env)
+        env = tmp
+        assert kwargs.get('env') is None
+        kwargs['env'] = env
+    result = subprocess.run(cmd, *args, **kwargs, check=True)
+    if result.returncode != 0:
+        print(f"Command failed: {' '.join(result.stderr)}")
+        raise subprocess.CalledProcessError(result.returncode, cmd)
+    return result
